bcaf806c651cce19b2f9fa64e3d7f15a3f871212
Tomasz Kula <tomaszkulawebdev@gmail.com>
2019-01-13 15:07:13 +0100

    add polyfill for stackblitz demo

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -41,7 +41,7 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -77,8 +77,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
